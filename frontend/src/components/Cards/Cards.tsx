c6c1a1f3a1dfce93b983c1baf119feb584b50352
chrluu97 <chrluu.dtt@gmail.com>
2023-07-29 15:06:15 +1000

    Format card layout and hover effect. add to packages

--- frontend/src/components/Cards/Cards.tsx
+++ frontend/src/components/Cards/Cards.tsx
@@ -1,19 +1,25 @@
-import React from 'react';
-import { cardData } from './cardData';
+import React from "react";
+import { cardData } from "./cardData";
 
-export function Cards(){
-    return (
-        <div className='grid grid-cols-4 gap-4 bg-gray-800'>
-            {cardData.map((card, index) => (
-                    <div className="w-48 h-32" key={index}>
-                        <img
-                            src={`${card.image}`}
-                            alt={`Card ${index + 1}`}
-                            className="w-48 h-32" // Set the width and height according to your desired size
-                        />
-                    <p className="text-white">{card.description}</p>
-                    </div>
-                ))}
+export function Cards() {
+  return (
+    <div className="grid grid-cols-4 gap-20 bg-gray-800">
+      {cardData.map((card, index) => (
+        <div
+          className="w-48 h-32 flex flex-col justify-between transform transition-transform hover:scale-105 cursor-pointer"
+          key={index}
+        >
+          <img
+            src={`${card.image}`}
+            alt={`Card ${index + 1}`}
+            className="w-48 h-32" // Set the width and height according to your desired size
+          />
+          <div className="text-white mt-2relative">
+            <p className="relative z-10">{card.description}</p>
+            <div className="absolute bottom-0 left-0 w-0 h-2 bg-white transition-width duration-300 ease-in-out group-hover:w-full"></div>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
